720f990a8396498255b3c848334d2de1f6a1422c
Kyu Nam Cho <mystous@naver.com>
2019-04-17 15:42:31 +0900

    Namespace 추가 및 생성자 초기화
    Review시 언급 되었던 Namespace와 멤버 변수 초기화를 생성자에서 진행하는 건 코드 수정하였습니다.

--- project2/goods.h
+++ project2/goods.h
@@ -1,6 +1,8 @@
 #ifndef GOODS_H_
 #define GOODS_H_
 
+namespace myOwn{
+ 
 // Goods class for representing goods information with index and price
 // Example:
 //    Goods test;
@@ -8,14 +10,18 @@
 //    std::cout << test.GetIndex() << " - " << test.GetPrice << endl;
 class Goods{
  public:
+ 
+  Goods() : goods_index_(0), goods_price_(0)
   void SetGoodsInfo(int index, int price);
   int GetIndex();
   int GetPrice();
 
  private:
-  int goods_index_ = 0;
-  int goods_price_ = 0;
+  int goods_index_;
+  int goods_price_;
 };
+ 
+}
 
 
 #endif  // GOODS_H_
